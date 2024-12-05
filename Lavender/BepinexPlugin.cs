b6f1a9941de8392519a23d22d86144b55b2fd6a8
BoettcherDasOriginal <boettcherleonard@gmail.com>
2024-12-05 20:15:18 +0100

    bare bones BepinexPlugin

--- Lavender/BepinexPlugin.cs
+++ Lavender/BepinexPlugin.cs
@@ -1,10 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using BepInEx;
+using BepInEx.Logging;
 
 namespace Lavender
 {
-    class BepinexPlugin
+    [BepInPlugin(LCMPluginInfo.PLUGIN_GUID, LCMPluginInfo.PLUGIN_NAME, LCMPluginInfo.PLUGIN_VERSION)]
+    class BepinexPlugin : BaseUnityPlugin
     {
+        internal static ManualLogSource Log = null!;
+
+        private void Awake()
+        {
+            Log = Logger;
+
+            // Log our awake here so we can see it in LogOutput.txt file
+            Log.LogInfo($"Plugin {LCMPluginInfo.PLUGIN_NAME} version {LCMPluginInfo.PLUGIN_VERSION} is loaded!");
+        }
     }
 }
